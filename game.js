108681b95f1b1629f7b53ed64a285fce93216880
tmullin6 <tyleramullins@gmail.com>
2021-04-11 17:42:17 -0600

    Updated game logic in main game function and added functionality to replay the game after game over

--- game.js
+++ game.js
@@ -1,62 +1,47 @@
-let comScore = 0 
-let playerScore = 0
+/*Project #2 for The Odin Project. Create a simple Rock Paper Scissors game to be played in the browser through the console. 
+The game is played by a player against the computer. The computer makes a move choice randomly and the move is compared to the players input.
+The game funs five rounds and a winner is determined. */
 
-let round =1
 
-while (true) {
+
+
+let comScore = 0        //Initialize Computer score to zero
+let playerScore = 0     //Initialize Player score to zero
+let play=true
+
+
+while(play==true) {
     game()
-    break
+    replay()
 }
 
-
-function game() {
-
-    while (round <= 5 ) {
-        let playerMove = playerChoice()
-        let comMove = comChoice()
     
+
+
+
+function game() {                                           //Initialize a function called "Game" that contains the main game logic and structure
+     
+        
+let round=1
+
+    while (round <=5) {
+        let playerMove = playerChoice().trim()
+        let comMove = comChoice().trim()
         Compare(playerMove, comMove)
         console.log(`You chose: ${playerMove}`)
         console.log(`Computer chose: ${comMove}`)
         console.log(`Player: ${playerScore}`)
         console.log(`Computer: ${comScore}`)
         round +=1
-    }
-    
-    if (comScore < playerScore) {
-        console.log("You Win!")
-    }
-    
-    else {
-        console.log("You lose this time. Try again!")
-    }
-
-    let playAgain = prompt("Would you like to play again? Y/N")
-
-    if (playAgain=="Y" || playAgain=="y") {
-        return true;
-    }
-
-    else if(playAgain=="N" || playAgain=="n") {
-        alert("Thanks for playing!")
-        return false;
-    }
-
-    else {
-        alert("Please make a valid selection")
-    }
-
-    
+}
+    findWinner()
 
 }
-function comChoice() {
-    let choice = Math.floor(Math.random()*4)
 
-    if(choice==0) {
-        choice="rock"
-    }
+function comChoice() {                                      //Initialize a function for the computer's move choice. Uses RNG to determine the move and returns value to game   
+    let choice = Math.floor(Math.random()*3)+1
 
-    else if (choice==1) {
+    if (choice==1) {
         choice="rock"
     }
 
@@ -71,36 +56,86 @@ function comChoice() {
     return choice
 }
 
-function playerChoice() {
+function playerChoice() {                                   //Initialize a function to get a choice from the player and return the value to the game
 
     let choice = prompt("Please choose Rock, Paper or Scissors").toLowerCase()
+
+    //if (choice!= "rock" || choice!= "paper" || choice !="scissors") {
+      //  alert("Please choose ONLY Rock, Paper, or Scissors!")
+        //choice = prompt("Please choose Rock, Paper or Scissors").toLowerCase()
+    //}
 
     return choice
 }
 
-function Compare(playerMove, comMove) {
+function Compare(playerMove, comMove) {                     //Initialize a function that compares the player's and computer's and updates score variable for winner
 
     if (playerMove=="rock" && comMove =="scissors") {
-        alert("You win!")
         playerScore +=1
+        alert(`You win! \n Player: ${playerScore} \n Computer: ${comScore}`)
+        
     }
     else if (playerMove=="scissors" && comMove =="paper") {
-        alert("You win!")
         playerScore +=1
+        alert(`You win! \n Player: ${playerScore} \n Computer: ${comScore}`)
+        
     }
 
     else if (playerMove=="paper" && comMove =="rock") {
-        alert("You win!")
         playerScore +=1
+        alert(`You win! \n Player: ${playerScore} \n Computer: ${comScore}`)
+        
     }
     else if(playerMove==comMove) {
-        alert("You Tied!")
+        alert(`You tied! \n Player: ${playerScore} \n Computer: ${comScore}`)
     }
 
     else {
-        alert("You lost. Try again")
         comScore+=1
+        alert(`You lost! \n Player: ${playerScore} \n Computer: ${comScore}`)
+        
+    }
+   
+}
+
+function findWinner() {                                       //Takes results from the game and determines a final winner
+
+    if (comScore < playerScore) {
+        alert(`You won! \n Player: ${playerScore} \n Computer: ${comScore}`)
+    }
+    
+    else if (comScore>playerScore){
+        alert(`You lose this time. Try again! \n Player: ${playerScore} \n Computer: ${comScore}`)
     }
 
+    else {
+        alert("A tie game! Try again!")
+    }
+
+}
+
+function replay() {                                           //Asks user if they would like to play again and restarts the game loop
+    
+    
+
    
+
+    let playAgain = prompt("Would you like to play again? Y/N")
+
+    if (playAgain=="Y".trim() || playAgain=="y".trim()) {
+        comScore=0
+        playerScore=0
+        play=true
+        i=false
+    }
+    else if(playAgain=="N".trim() || playAgain=="n".trim()) {
+        alert("Thanks for playing!")
+        play=false
+        
+    }
+    else {
+        alert("Please make a proper selection!")
+        
+    }
+
 }

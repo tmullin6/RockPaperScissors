b22b459188d6255c038cea4574358c6d3169a216
tmullin6 <tyleramullins@gmail.com>
2021-04-19 15:00:43 -0600

    Finished start screen logic

--- newgame.js
+++ newgame.js
@@ -1,8 +1,19 @@
 const title=document.querySelector("#title");
 const start=document.querySelector("#start");
 const desc = document.querySelector("#description");
+const gamearea = document.querySelector(".game-area");
+const moves= document.querySelector("#moves");
 
-start.addEventListener ('click', () => {
-    title.setAttribute('style', "margin-top: 0px; font-size: 24px;");
+startGame();
+
+
+function startGame() {
+    start.addEventListener ('click', () => {
+        title.setAttribute('style', "margin-top: 0px; font-size: 30px; text-shadow: none;");
+        desc.textContent="Select your weapon"
+        desc.setAttribute("style", "margin-top: 5px; color: red; font-size: 20px;")
+        gamearea.removeChild(start);
+        moves.classList.toggle("player-moves");
     
-})
+    });   
+}

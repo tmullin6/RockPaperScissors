35e37b06e1a41115889039d85a466ca40549fac4
tmullin6 <tyleramullins@gmail.com>
2021-04-18 19:12:05 -0600

    Added style to title screen and started work on newgame.js

---
+++ newgame.js
@@ -0,0 +1,8 @@
+const title=document.querySelector("#title");
+const start=document.querySelector("#start");
+const desc = document.querySelector("#description");
+
+start.addEventListener ('click', () => {
+    title.setAttribute('style', "margin-top: 0px; font-size: 24px;");
+    
+})
